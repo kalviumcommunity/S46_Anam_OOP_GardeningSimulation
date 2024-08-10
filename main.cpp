04ec17a3c2bbe2778e4b0092ceb06ae2bfd2c79a
Anam-Ashraf7 <anam.ashraf.7357@gmail.com>
2024-08-10 15:21:52 +0530

    Created class and objects

---
+++ main.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Plant{
+
+    protected:
+        string name;
+        string growthStage;
+        double age;
+        int health;
+        int matureAge;
+        int maxWater;
+        int waterLevel;
+        int time;
+
+    public:
+
+        Plant(string name, int matureAge){
+            this->name = name;
+            this->matureAge = matureAge;
+            this->maxWater = 100;
+            this->waterLevel = 0;
+            this->health = 0;
+            this->age = 0;
+            this->growthStage = "Seedling";
+        }
+        void grow(){
+            if (growthStage == "Seedling") {
+                if (time >= matureAge / 2) {
+                    growthStage = "Growing";
+                    health += 20;
+                }
+            } else if (growthStage == "Growing") {
+                if (age >= matureAge) {
+                    growthStage = "Mature";
+                    health += 15;
+                }
+            }
+            if (age > matureAge) {
+                health -= 5;
+            } else {
+                health += 10;
+            }
+            age += 0.5;
+            time += 1;
+        }
+        void water(int amount){
+            waterLevel = min(maxWater, waterLevel + amount);
+        }
+        void status(){
+            cout << "Plant: " << name << endl;
+            cout << "Growth Stage: " << growthStage << endl;
+            cout << "Age: " << age << endl;
+            cout << "Health: " << health << endl;
+            cout << "Water Level: " << waterLevel << endl;
+        }
+};
+
+class Garden {
+    private:
+        vector<Plant> plants;
+
+    public:
+        void addPlant(const Plant& p){
+            plants.push_back(p);
+        }
+        void waterAllPlants(int amount) {
+            for (auto& plant : plants) {
+                plant.water(amount);
+            }
+        }
+        void growAllPlants() {
+            for (auto& plant : plants) {
+                plant.grow();
+            }
+        }
+        void showAllPlants(){
+            for (auto& plant : plants) {
+                plant.status();
+                cout << "----------------" << endl;
+            }
+        }
+};
+
+
+int main() {
+    Garden myGarden;
+
+    Plant rose("Rose", 12);
+    Plant tomato("Tomato", 8);
+
+    myGarden.addPlant(rose);
+    myGarden.addPlant(tomato);
+
+    cout << "Initial Plant States:\n";
+    myGarden.showAllPlants();
+
+    myGarden.waterAllPlants(20);
+    myGarden.growAllPlants();
+
+    cout << "\nUpdated Plant States:\n";
+    myGarden.showAllPlants();
+
+    return 0;
+}
